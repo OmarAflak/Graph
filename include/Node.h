68ee6ef5fe2fac3340d3dae0c92cde86b9e4241d
Omar Aflak <aflakomar@gmail.com>
2018-03-01 23:14:18 +0100

    added connections count in Node

--- include/Node.h
+++ include/Node.h
@@ -6,12 +6,16 @@
 class Node{
 	private:
 		std::string m_name;
+		int m_connections;
 
 	public:
 		Node();
 		Node(std::string name);
 
 		std::string getName() const;
+		int getConnections() const;
+
+		void setConnections(int connection);
 };
 
 #endif
--- src/Node.cpp
+++ src/Node.cpp
@@ -11,3 +11,11 @@ Node::Node(){
 std::string Node::getName() const{
 	return m_name;
 }
+
+int Node::getConnections() const{
+	return m_connections;
+}
+
+void Node::setConnections(int connections){
+	m_connections = connection;
+}
