aa9241f2cab4c56fa7c55bcb0ac19a8d4f0ab96c
Omar Aflak <aflakomar@gmail.com>
2018-03-03 17:45:04 +0100

    added comments in main

--- src/main.cpp
+++ src/main.cpp
@@ -4,29 +4,40 @@
 
 using namespace std;
 
-void createGraph(const char* filename){
+Graph createGraph(){
 	Graph graph;
-
-	// Nodes are created implicitly when connecting them together
+	
+	// Nodes are created implicitly when using connect()
 	graph.connect("Emmanuel M", "François B");
 	graph.connect("Manuel V", "Christiane T");
 	graph.connect("Manuel V", "Alain J");
 	graph.connect("François B", "Emmanuel M");
 	graph.connect("Marine L", "Manuel V");
+	return graph;
+}
 
+void saveGraph(const Graph &graph, const char* filename){
 	ofstream out(filename);
 	out << graph;
 	out.close();
 }
 
-void loadGraph(const char* filename){
+Graph loadGraph(const char* filename){
 	Graph graph;
 	ifstream in(filename);
 	in >> graph;
-	graph.print();
+	return graph;
 }
 
 int main(){
-	createGraph("graph");
-	loadGraph("graph");
+	// Create graph
+	Graph graph = createGraph();
+
+	// Save it
+	saveGraph(graph, "graph.txt");
+
+	// Load it
+	// You can edit graph.txt manually and comment the lines above
+	Graph _graph = loadGraph("graph.txt");
+	_graph.print();
 }
