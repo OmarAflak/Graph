bb307e55b66ea677b53914e3a79beccba0b19b9e
mada <madaaf123@gmail.com>
2018-04-20 18:58:36 +0200

    changed rand method

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,15 +1,14 @@
 #include "../include/utils.h"
 
 std::string genUid(size_t length){
-    static const std::string::value_type allowed_chars[] {"123456789ABCDFGHJKLMNPQRSTVWXZabcdfghjklmnpqrstvwxz"};
-
-    static thread_local std::default_random_engine randomEngine(std::random_device{}());
-    static thread_local std::uniform_int_distribution<int> randomDistribution(0, sizeof(allowed_chars) - 1);
-
+    std::string allowed_chars = "123456789ABCDFGHJKLMNPQRSTVWXZabcdfghjklmnpqrstvwxz";
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(0, sizeof(allowed_chars)-1);
     std::string id(length ? length : 16, '\0');
 
     for (std::string::value_type& c : id) {
-        c = allowed_chars[randomDistribution(randomEngine)];
+        c = allowed_chars[dis(gen)];
     }
 
     return id;
